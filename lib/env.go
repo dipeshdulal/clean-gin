c166450c1ec027e3421f409c64ed7c3a5b442287
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-12 21:08:09 +0545

    feat: update environment variables

--- lib/env.go
+++ lib/env.go
@@ -4,15 +4,21 @@ import "os"
 
 // Env has environment stored
 type Env struct {
-	ServerPort string
+	ServerPort  string
+	Environment string
+	LogOutput   string
 }
 
 // NewEnv creates a new environment
 func NewEnv() Env {
-	return Env{}
+	env := Env{}
+	env.LoadEnv()
+	return env
 }
 
 // LoadEnv loads environment
 func (env *Env) LoadEnv() {
 	env.ServerPort = os.Getenv("ServerPort")
+	env.Environment = os.Getenv("Environment")
+	env.LogOutput = os.Getenv("LogOutput")
 }
