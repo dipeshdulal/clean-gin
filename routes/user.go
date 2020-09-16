20332a73c7f6b9a909eddd4275de2bd6895a6802
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-09-16 18:36:20 +0545

    feat: implement route group wise middleware

--- routes/user.go
+++ routes/user.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/dipeshdulal/clean-gin/controllers"
 	"github.com/dipeshdulal/clean-gin/lib"
+	"github.com/dipeshdulal/clean-gin/middlewares"
 )
 
 // UserRoutes struct
@@ -10,12 +11,13 @@ type UserRoutes struct {
 	logger         lib.Logger
 	handler        lib.RequestHandler
 	userController controllers.UserController
+	authMiddleware middlewares.JWTAuthMiddleware
 }
 
 // Setup user routes
 func (s UserRoutes) Setup() {
 	s.logger.Zap.Info("Setting up routes")
-	api := s.handler.Gin.Group("/api")
+	api := s.handler.Gin.Group("/api").Use(s.authMiddleware.Handler())
 	{
 		api.GET("/user", s.userController.GetUser)
 		api.GET("/user/:id", s.userController.GetOneUser)
@@ -26,10 +28,16 @@ func (s UserRoutes) Setup() {
 }
 
 // NewUserRoutes creates new user controller
-func NewUserRoutes(handler lib.RequestHandler, userController controllers.UserController, logger lib.Logger) UserRoutes {
+func NewUserRoutes(
+	logger lib.Logger,
+	handler lib.RequestHandler,
+	userController controllers.UserController,
+	authMiddleware middlewares.JWTAuthMiddleware,
+) UserRoutes {
 	return UserRoutes{
 		handler:        handler,
 		logger:         logger,
 		userController: userController,
+		authMiddleware: authMiddleware,
 	}
 }
