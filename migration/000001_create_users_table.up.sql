0cb5bb4900f563b613f91dbd3f53bb9d17bc1e2c
dipesh <kcdipesh429@gmail.com>
2020-12-03 18:21:56 +0545

    feat:add user migration prototype

---
+++ migration/000001_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,  
+  `email` VARCHAR(100) NOT NULL,
+  `first_name` VARCHAR(20) NOT NULL,
+  `last_name` VARCHAR(20) NOT NULL,
+  `user_name` VARCHAR(20) NOT NULL,
+  `uid` VARCHAR(30) NOT NULL,
+  `stripe_account_id` VARCHAR(100) NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
