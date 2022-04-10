d80cbf6fdc905772d69371a2c29636c6f003281b
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2022-04-10 14:19:51 +0545

    feat: logger update to accomodate log_level env

--- lib/logger.go
+++ lib/logger.go
@@ -2,9 +2,11 @@ package lib
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"time"
 
+	"go.uber.org/fx/fxevent"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 
@@ -37,7 +39,7 @@ var (
 // GetLogger get the logger
 func GetLogger() Logger {
 	if globalLogger == nil {
-		logger := newLogger()
+		logger := newLogger(NewEnv())
 		globalLogger = &logger
 	}
 	return *globalLogger
@@ -53,14 +55,78 @@ func (l Logger) GetGinLogger() GinLogger {
 	}
 }
 
-// GetFxLogger get the fx logger
-func (l Logger) GetFxLogger() FxLogger {
+// GetFxLogger gets logger for go-fx
+func (l *Logger) GetFxLogger() fxevent.Logger {
 	logger := zapLogger.WithOptions(
 		zap.WithCaller(false),
 	)
+	return &FxLogger{Logger: newSugaredLogger(logger)}
+}
 
-	return FxLogger{
-		Logger: newSugaredLogger(logger),
+// LogEvent log event for fx logger
+func (l *FxLogger) LogEvent(event fxevent.Event) {
+	switch e := event.(type) {
+	case *fxevent.OnStartExecuting:
+		l.Logger.Debug("OnStart hook executing: ",
+			zap.String("callee", e.FunctionName),
+			zap.String("caller", e.CallerName),
+		)
+	case *fxevent.OnStartExecuted:
+		if e.Err != nil {
+			l.Logger.Debug("OnStart hook failed: ",
+				zap.String("callee", e.FunctionName),
+				zap.String("caller", e.CallerName),
+				zap.Error(e.Err),
+			)
+		} else {
+			l.Logger.Debug("OnStart hook executed: ",
+				zap.String("callee", e.FunctionName),
+				zap.String("caller", e.CallerName),
+				zap.String("runtime", e.Runtime.String()),
+			)
+		}
+	case *fxevent.OnStopExecuting:
+		l.Logger.Debug("OnStop hook executing: ",
+			zap.String("callee", e.FunctionName),
+			zap.String("caller", e.CallerName),
+		)
+	case *fxevent.OnStopExecuted:
+		if e.Err != nil {
+			l.Logger.Debug("OnStop hook failed: ",
+				zap.String("callee", e.FunctionName),
+				zap.String("caller", e.CallerName),
+				zap.Error(e.Err),
+			)
+		} else {
+			l.Logger.Debug("OnStop hook executed: ",
+				zap.String("callee", e.FunctionName),
+				zap.String("caller", e.CallerName),
+				zap.String("runtime", e.Runtime.String()),
+			)
+		}
+	case *fxevent.Supplied:
+		l.Logger.Debug("supplied: ", zap.String("type", e.TypeName), zap.Error(e.Err))
+	case *fxevent.Provided:
+		for _, rtype := range e.OutputTypeNames {
+			l.Logger.Debug("provided: ", e.ConstructorName, " => ", rtype)
+		}
+	case *fxevent.Decorated:
+		for _, rtype := range e.OutputTypeNames {
+			l.Logger.Debug("decorated: ",
+				zap.String("decorator", e.DecoratorName),
+				zap.String("type", rtype),
+			)
+		}
+	case *fxevent.Invoking:
+		l.Logger.Debug("invoking: ", e.FunctionName)
+	case *fxevent.Started:
+		if e.Err == nil {
+			l.Logger.Debug("started")
+		}
+	case *fxevent.LoggerInitialized:
+		if e.Err == nil {
+			l.Logger.Debug("initialized: custom fxevent.Logger -> ", e.ConstructorName)
+		}
 	}
 }
 
@@ -86,19 +152,37 @@ func newSugaredLogger(logger *zap.Logger) *Logger {
 }
 
 // newLogger sets up logger
-func newLogger() Logger {
+func newLogger(env Env) Logger {
 
 	config := zap.NewDevelopmentConfig()
-	env := os.Getenv("ENV")
 	logOutput := os.Getenv("LOG_OUTPUT")
 
-	if env == "development" {
+	if env.Environment == "development" {
+		fmt.Println("encode level")
 		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	}
 
-	if env == "production" && logOutput != "" {
+	if env.Environment == "production" && logOutput != "" {
 		config.OutputPaths = []string{logOutput}
 	}
+
+	logLevel := os.Getenv("LOG_LEVEL")
+	level := zap.PanicLevel
+	switch logLevel {
+	case "debug":
+		level = zapcore.DebugLevel
+	case "info":
+		level = zapcore.InfoLevel
+	case "warn":
+		level = zapcore.WarnLevel
+	case "error":
+		level = zapcore.ErrorLevel
+	case "fatal":
+		level = zapcore.FatalLevel
+	default:
+		level = zap.PanicLevel
+	}
+	config.Level.SetLevel(level)
 
 	zapLogger, _ = config.Build()
 	logger := newSugaredLogger(zapLogger)
@@ -114,7 +198,10 @@ func (l GinLogger) Write(p []byte) (n int, err error) {
 
 // Printf prits go-fx logs
 func (l FxLogger) Printf(str string, args ...interface{}) {
-	l.Infof(str, args)
+	if len(args) > 0 {
+		l.Debugf(str, args)
+	}
+	l.Debug(str)
 }
 
 // GORM Framework Logger Interface Implementations
